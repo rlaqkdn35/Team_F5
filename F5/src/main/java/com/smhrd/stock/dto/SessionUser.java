2b30a8ebd69b49353acc7faa8e23d7e754d12796
1stph <guswls6138@gmail.com>
2025-06-05 11:41:58 +0900

    박현진
    Sessiondto

---
+++ F5/src/main/java/com/smhrd/stock/dto/SessionUser.java
@@ -0,0 +1,11 @@
+package com.smhrd.stock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SessionUser {
+    private String email;
+    private String nickname;
+}
